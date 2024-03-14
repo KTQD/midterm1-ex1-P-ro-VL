566083698461d3147085ce51f293a25d46cc7264
Phạm Văn Linh <inyourarea2802@gmail.com>
2024-03-14 07:44:20 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3 = new MP3("mp3", "Music File",
-                "This is an MP3 music file", 100, 10);
+        MP3 mp3 = new MP3("mp3", "Music File", "This is an MP3 music file", 100, 10);
         mp3.showInfo();
 
-        Book book = new Book("book", "Harry Potter",
-                "This is a book", 100,
-                "J.K.Rowling", 500, "novel");
+        Book book = new Book("book", "Harry Potter", "This is a book", 100, "J.K.Rowling", 500, "novel");
         book.showInfo();
 
     }
